75f1cf3532e25ee928bb6c85f09bfdf2a9a9bd6c
porter <bloodpollution@live.com>
2022-03-22 04:13:43 -0800

    fundamentals of object oriented programming language

---
+++ src/day1/Dustbin.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Dustbin {
+    //state (variables,constants)
+    //color
+    //weight
+    //height
+    //closed door
+    //open
+    //metal
+    //maximum weight
+    //location
+
+    //Behaviour (function or method)
+    //collect reuseable material,glass,metal
+    //
+
+}
---
+++ src/day1/Human.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class Human {
+
+    //states
+    //black
+    //asin
+    //white
+    //child
+    //adult
+    //old
+    //deff
+    //blind
+    //disable
+
+    //behaviour
+     // cry
+    //happy
+    //developer
+    //don
+    //drunker
+}
+
