f92167771fdb789802a6159b1ebf8a54cbc67bf3
porter <bloodpollution@live.com>
2022-03-22 02:59:20 -0800

    initial commit

---
+++ src/Welcome.java
@@ -0,0 +1,14 @@
+
+    public class Welcome{
+        public static void main(String [] args){
+            String greeting = "welcome to java from intellije";
+            System.out.println(greeting);
+            for(int i=0;i<greeting.length();i++){
+                System.out.println("=");
+            }
+            System.out.println();
+
+
+        }
+    }
+
